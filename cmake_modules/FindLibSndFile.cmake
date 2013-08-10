c1e8572a6fcaf0fb24612439a7500291246cac8e
Andres Cabrera <andres@lenovo-W530>
2013-08-10 14:59:36 -0700

    Removed support for slashes which caused dependency search to fail. Added new method with underscores

--- cmake_modules/FindLibSndFile.cmake
+++ cmake_modules/FindLibSndFile.cmake
@@ -1,90 +1,41 @@
-# Base Io build system
-# Written by Jeremy Tregunna <jeremy.tregunna@me.com>
-#
-# Find libsndfile.
-
-#FIND_PATH(LIBSNDFILE_INCLUDE_DIR sndfile.h)
-
-#SET(LIBSNDFILE_NAMES ${LIBSNDFILE_NAMES} sndfile libsndfile)
-#FIND_LIBRARY(LIBSNDFILE_LIBRARY NAMES ${LIBSNDFILE_NAMES} PATH)
-
-#IF(LIBSNDFILE_INCLUDE_DIR AND LIBSNDFILE_LIBRARY)
-#	SET(LIBSNDFILE_FOUND TRUE)
-#ENDIF(LIBSNDFILE_INCLUDE_DIR AND LIBSNDFILE_LIBRARY)
-
-#IF(LIBSNDFILE_FOUND)
-#	IF(NOT LibSndFile_FIND_QUIETLY)
-#		MESSAGE(STATUS "Found LibSndFile: ${LIBSNDFILE_LIBRARY}")
-#	ENDIF (NOT LibSndFile_FIND_QUIETLY)
-#ELSE(LIBSNDFILE_FOUND)
-#	IF(LibSndFile_FIND_REQUIRED)
-#		MESSAGE(FATAL_ERROR "Could not find sndfile")
-#	ENDIF(LibSndFile_FIND_REQUIRED)
-#ENDIF (LIBSNDFILE_FOUND)
-
-
-# - Try to find libsndfile
+# - Try to find SNDFILE
 # Once done this will define
+#
+#  SNDFILE_FOUND - system has SNDFILE
+#  SNDFILE_INCLUDE_DIR - the SNDFILE include directory
+#  SNDFILE_LIBRARIES - Link these to use SNDFILE
+#
 
-#  SNDFILE_FOUND - system has libsndfile
-#  SNDFILE_INCLUDE_DIRS - the libsndfile include directory
-#  SNDFILE_LIBRARIES - Link these to use libsndfile
+include(LibFindMacros)
 
-#  Copyright (C) 2006  Wengo
+# Dependencies
+#libfind_package(LO Magick)
 
-#  Redistribution and use is allowed according to the terms of the New
-#  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(SNDFILE_PKGCONF libsndfile)
 
-
-if (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
-  # in cache already
-  set(SNDFILE_FOUND TRUE)
-else (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
-
-  find_path(SNDFILE_INCLUDE_DIR
-    NAMES
-      sndfile.h
-    PATHS
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /sw/include
-  )
-
-  find_library(SNDFILE_LIBRARY
-    NAMES
-      sndfile
-      libsndfile
-    PATHS
+# Include dir
+find_path(SNDFILE_INCLUDE_DIR
+  NAMES sndfile.h
+  PATHS ${SNDFILE_PKGCONF_INCLUDE_DIRS} 
       /usr/lib
       /usr/local/lib
       /opt/local/lib
       /sw/lib
-  )
+)
 
-  set(SNDFILE_INCLUDE_DIRS
-    ${SNDFILE_INCLUDE_DIR}
-  )
-  set(SNDFILE_LIBRARIES
-    ${SNDFILE_LIBRARY}
-  )
+# Finally the library itself
+find_library(SNDFILE_LIBRARY
+  NAMES sndfile libsndfile
+  PATHS ${SNDFILE_PKGCONF_LIBRARY_DIRS}
+      /usr/lib
+      /usr/local/lib
+      /opt/local/lib
+      /sw/lib
+)
 
-  if (SNDFILE_INCLUDE_DIRS AND SNDFILE_LIBRARIES)
-    set(SNDFILE_FOUND TRUE)
-  endif (SNDFILE_INCLUDE_DIRS AND SNDFILE_LIBRARIES)
-
-  if (SNDFILE_FOUND)
-    if (NOT SndFile_FIND_QUIETLY)
-      message(STATUS "Found libsndfile: ${SNDFILE_LIBRARIES}")
-    endif (NOT SndFile_FIND_QUIETLY)
-  else (SNDFILE_FOUND)
-    if (SndFile_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find libsndfile")
-    endif (SndFile_FIND_REQUIRED)
-  endif (SNDFILE_FOUND)
-
-  # show the SNDFILE_INCLUDE_DIRS and SNDFILE_LIBRARIES variables only in the advanced view
-  mark_as_advanced(SNDFILE_INCLUDE_DIRS SNDFILE_LIBRARIES)
-
-endif (SNDFILE_LIBRARIES AND SNDFILE_INCLUDE_DIRS)
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(SNDFILE_PROCESS_INCLUDES SNDFILE_INCLUDE_DIR)
+set(SNDFILE_PROCESS_LIBS SNDFILE_LIBRARY)
+libfind_process(SNDFILE)
