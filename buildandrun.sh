0084ebd4f304741b1764254db0b2c281dff69b6f
Andres Cabrera <andres@lenovo-W530>
2013-08-27 22:14:34 -0700

    Improved buildandrun.sh script to detect whether targets exist to not rerun cmake

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,24 +1,29 @@
 
 
 FILENAME=`echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`
+TARGET=${FILENAME}_run
 
 if [ ! -f Makefile ]; then
-    .distclean
+    ./distclean
     cmake .
     if [ $? != 0 ]; then
 	exit 1
     fi
 fi 
 
-make `echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`_run -j4 $*
+make $TARGET -j4 $*
 
 # If command failed, try again after running cmake (in case it is a new target)
 if [ $? != "0" ]
     then
-    echo "Can't find target --------------- Running CMAKE again"
-    cmake .
-    if [ $? != 0 ]; then
-	exit 1
-    fi
-    make `echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`_run -j4 $*
+    TARGETS=`make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}'`
+    case $TARGETS in
+	*"$TARGET"*) echo "Target exists." ;;
+	*) echo "Can't find target --------------- Running CMAKE again"
+	    cmake .
+	    if [ $? != 0 ]; then
+		exit 1
+	    fi
+	    make `echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`_run -j4 $* ;;
+    esac
 fi
